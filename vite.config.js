c0d262f2e06952eab3eacac991dcbf5a4a9557d8
jesusmacias0320 <jesusmacias0320@gmail.com>
2025-10-16 01:54:40 -0400

    commit inicial

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jdmacias_shopping_plants/',
+  base: '/jdmacias0320-e-plantShopping/',
   plugins: [react()],
 })
